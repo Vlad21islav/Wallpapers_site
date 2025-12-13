9cea872ea636a9691798c0136b607bc10538a4e0
Vladislav <vmitk@mail.ru>
2025-12-13 19:43:52 +0300

    script_bugs

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let images = [
-    {'name': 'красный закат', 'path': 'images/image1.jpg', 'category': 'Природа'}, 
-    {'name': 'синие горы', 'path': 'images/image2.jpg', 'category': 'Природа'}, 
-    {'name': 'пейзаж гор и озера', 'path': 'images/image3.jpg', 'category': 'Природа'}, 
-    {'name': 'туманный вид на деревню', 'path': 'images/image4.jpg', 'category': 'Природа'},
-    {'name': 'серая абстракция', 'path': 'images/image5.jpg', 'category': 'Абстракция'},
-    {'name': 'minecraft закат', 'path': 'images/image6.jpg', 'category': 'Игры'},
-    {'name': 'minecraft река', 'path': 'images/image7.jpg', 'category': 'Игры'},
-    {'name': 'туманный лес', 'path': 'images/image8.jpg', 'category': 'Природа'},
-    {'name': 'чёрная абстракция', 'path': 'images/image9.jpg', 'category': 'Абстракция'},
+    {name: 'красный закат', path: 'images/image1.jpg', category: 'Природа'}, 
+    {name: 'синие горы', path: 'images/image2.jpg', category: 'Природа'}, 
+    {name: 'пейзаж гор и озера', path: 'images/image3.jpg', category: 'Природа'}, 
+    {name: 'туманный вид на деревню', path: 'images/image4.jpg', category: 'Природа'},
+    {name: 'серая абстракция', path: 'images/image5.jpg', category: 'Абстракция'},
+    {name: 'minecraft закат', path: 'images/image6.jpg', category: 'Игры'},
+    {name: 'minecraft река', path: 'images/image7.jpg', category: 'Игры'},
+    {name: 'туманный лес', path: 'images/image8.jpg', category: 'Природа'},
+    {name: 'чёрная абстракция', path: 'images/image9.jpg', category: 'Абстракция'},
 ];
 
 let categories = ['Все', 'Природа', 'Абстракция', 'Игры']
